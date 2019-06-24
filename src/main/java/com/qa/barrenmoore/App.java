cdce2992f18c388ec770f6afe90d62d47740f094
tvaidotas <tadas.vaidotas@gmail.com>
2019-06-24 14:30:30 +0100

    added exit as a command, can check location now

--- src/main/java/com/qa/barrenmoore/App.java
+++ src/main/java/com/qa/barrenmoore/App.java
@@ -1,22 +1,30 @@
 package com.qa.barrenmoore;
 
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App
 {
     // grid 3 by 3
-    private int x =2;
-    private int y =2;
+    private static int x =2;
+    private static int y =2;
 
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         boolean exit = false;
         while (!exit){
-            System.out.println("Actions available: location, check, east, west, north, south");
+            System.out.println("Actions available: location, check, east, west, north, south, exit");
             System.out.println("Please type what would you like to do:");
-            String userInput = System.console().readLine();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String userInput = reader.readLine();
+            if(userInput.toLowerCase().equals("location")){
+                printLocation();
+            }
         }
     }
 
-    private void printLocation(){
+    private static void printLocation(){
         System.out.println("You are at x:" + x + ", y:" + y);
     }
 }
