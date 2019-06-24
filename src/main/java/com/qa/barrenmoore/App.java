c7d62a4b60ed42dd1fb044dc70fc5e73ee15b7a2
tvaidotas <tadas.vaidotas@gmail.com>
2019-06-24 14:58:32 +0100

    randomly place the treasure within the 3x3 grid added compass to check how far from treasure are you

--- src/main/java/com/qa/barrenmoore/App.java
+++ src/main/java/com/qa/barrenmoore/App.java
@@ -3,6 +3,7 @@ package com.qa.barrenmoore;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 public class App
 {
@@ -10,14 +11,15 @@ public class App
     private static int x = 2;
     private static int y = 2;
 
-    private static int treasureX = 3;
-    private static int treasureY = 3;
+    private static int treasureX;
+    private static int treasureY;
 
     public static void main( String[] args ) throws IOException
     {
         boolean exit = false;
+        placeTreasure();
         while (!exit){
-            System.out.println("Actions available: location, check, east, west, north, south, exit");
+            System.out.println("Actions available: location, check, compass, east, west, north, south, exit");
             System.out.println("Please type what would you like to do:");
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String userInput = reader.readLine();
@@ -29,7 +31,11 @@ public class App
                 if (x == treasureX & y == treasureY){
                     System.out.println("Congratulations! You've found the treasure!");
                     exit = true;
+                } else {
+                    System.out.println("Nothing found");
                 }
+            } else if(userInput.toLowerCase().equals("compass")){
+                System.out.println("You are "+ Math.abs(((treasureX - x) + (treasureY -y)))+ " tiles away from the treasure");
             } else if (userInput.toLowerCase().equals("north")){
                 if (x - 1 > 0) {
                     x -= 1;
@@ -52,5 +58,11 @@ public class App
 
     private static void printLocation(){
         System.out.println("You are at x:" + x + ", y:" + y);
+    }
+
+    private static void placeTreasure(){
+        Random r = new Random();
+        treasureX = r.nextInt(4);
+        treasureY = r.nextInt(4);
     }
 }
