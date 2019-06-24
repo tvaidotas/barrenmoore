d77e36993b01a89fa96df0de63ecc858bd53316c
tvaidotas <tadas.vaidotas@gmail.com>
2019-06-24 14:17:41 +0100

    make the game run without stop

--- src/main/java/com/qa/barrenmoore/App.java
+++ src/main/java/com/qa/barrenmoore/App.java
@@ -1,13 +1,11 @@
 package com.qa.barrenmoore;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        boolean exit = false;
+        while (!exit){
+        }
     }
 }
