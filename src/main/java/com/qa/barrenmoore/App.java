b6b42fb315cc57211c03adcac74016e25b63546d
tvaidotas <tadas.vaidotas@gmail.com>
2019-06-24 14:13:29 +0100

    initial project structure

---
+++ src/main/java/com/qa/barrenmoore/App.java
@@ -0,0 +1,13 @@
+package com.qa.barrenmoore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
