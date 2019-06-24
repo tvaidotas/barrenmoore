5cda88ec971bcc132db6d6e766ce1722be7c71ab
tvaidotas <tadas.vaidotas@gmail.com>
2019-06-24 14:20:12 +0100

    add grid coordinates and set the spawn to the very middle for every game

--- src/main/java/com/qa/barrenmoore/App.java
+++ src/main/java/com/qa/barrenmoore/App.java
@@ -2,10 +2,19 @@ package com.qa.barrenmoore;
 
 public class App 
 {
+    // grid 3 by 3
+    private int x =2;
+    private int y =2;
+
     public static void main( String[] args )
     {
         boolean exit = false;
         while (!exit){
+
         }
+    }
+
+    private void printLocation(){
+        System.out.println("You are at x:" + x + ", y:" + y);
     }
 }
